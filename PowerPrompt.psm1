2df3aeb53ca5555e6840af3496e3d3ccce306e00
Paul Sammut <paulesammut@gmail.com>
2019-02-05 16:17:43 +1100

    store PowerPromptName in env. var.

--- PowerPrompt.psm1
+++ PowerPrompt.psm1
@@ -1,9 +1,34 @@
 #Author: psammut
-function PowerPrompt([string]$PromptName) {
+# if (-not (Test-Path env:PowerPromptName)) {
+#     $env:PowerPromptName = "PanelPrompt"
+#     [Environment]::SetEnvironmentVariable("PowerPromptName", "PanelPrompt", "User")
+# }
+$script:defaultPrompt="PanelPrompt"
 
-    & $PromptName
-    # PanelPrompt
+
+function PowerPrompt {
+
+    Param(
+        [string]$name,
+        [switch]$persist
+    )
+
+    if ($name) {
+        $Env:PowerPromptName=$name
+        if ($persist) {
+            [Environment]::SetEnvironmentVariable("PowerPromptName", "$name", "User")
+        }
+        Return
+    }
+
+    if (-not (Test-Path env:PowerPromptName)) {
+        $env:PowerPromptName = "$defaultPrompt"
+        [Environment]::SetEnvironmentVariable("PowerPromptName", "$defaultPrompt", "User")
+    }
+
+    & $Env:PowerPromptName
+
     Return "  "
-
 }
+
 Export-ModuleMember -Function PowerPrompt
